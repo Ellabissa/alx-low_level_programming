2232639ed2d6a7081a3a574b2c1dc568e83bf14a
Ellabissa <bissahfrimpongemmanuella@gmail.com>
2023-04-21 12:42:39 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - function to print combination of three digits.
- *
- * Return : 0 success
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int i = '0';
-int j = '0';
-int k = '0';
 
-while (i <= '7')
-{
-while (j <= '8')
-{
-while (k <= '9')
-{
-if (i < j && j < k)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (!(i == '7' && j == '8' && k == '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-k++;
-}
-k = '0';
-j++;
-}
-j = '0';
-i++;
-}
-putchar('\n');
-return (0);
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
+	{
+
+	for (p = d + 1; p <= '9'; p++)
+	{
+
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
